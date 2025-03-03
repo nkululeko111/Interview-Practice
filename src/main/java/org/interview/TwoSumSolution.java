6ae81c902afd5845016f29076cedf7f85ff77724
User <nkululekosekwanele@gmail.com>
2025-03-03 13:53:20 +0200

    Initial commit

---
+++ src/main/java/org/interview/TwoSumSolution.java
@@ -0,0 +1,40 @@
+package org.interview;
+
+import java.util.HashMap;
+
+
+/* **Problem Statement:**
+Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+**Example:**
+Input: `nums = [2, 7, 11, 15]`, `target = 9`
+Output: `[0, 1]`
+Explanation: `nums[0] + nums[1] = 2 + 7 = 9`. */
+
+public class TwoSumSolution {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
+
+
